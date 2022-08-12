8c7fec7dc55ba3410694059da8d79881747ac5f1
unknown <andresfelipehc@ufps.edu.co>
2022-08-12 17:00:48 -0500

    agregando un nuevo slice con reducers al store

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
-
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    counter: 10,
+    counter: 10
   },
   reducers: {
     increment: (state) => {
